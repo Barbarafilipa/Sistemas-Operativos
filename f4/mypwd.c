f39aff75885c6898d2319b1b5cca1a4efb125a2d
Barbarafilipa <barbarafilipacarvalho08@gmail.com>
2022-05-07 23:21:05 +0100

    ficha 4 feita

---
+++ f4/mypwd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+    if(argc != 1){
+        printf("usage: ./mypwd\n");
+        return 0;
+    }
+
+    size_t size = 255;
+    char buf[255];
+    getcwd(buf, size);
+    printf("%s\n", buf);
+
+    return EXIT_SUCCESS;
+}
